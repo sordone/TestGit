5e008ba93012d350a28dbee26cea28ad7da7a5e5
Sergio Ordóñez <sergio.ordonez@hotelerum.com>
2015-04-21 09:21:39 -0300

    Commit nro 2

--- TestGit/ViewController.m
+++ TestGit/ViewController.m
@@ -17,7 +17,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    /* Commit nro 1*/
+    /* Commit nro 2*/
     
     // Do any additional setup after loading the view, typically from a nib.
 }
